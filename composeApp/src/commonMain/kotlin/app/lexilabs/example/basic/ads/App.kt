f0683276dfeaf7c3a30bdd301dfa3e92fe5b3227
Robert Jamison <65142411+robertjamison@users.noreply.github.com>
2025-04-12 17:03:15 -0400

    Fixed Compose screen insets

--- composeApp/src/commonMain/kotlin/app/lexilabs/example/basic/ads/App.kt
+++ composeApp/src/commonMain/kotlin/app/lexilabs/example/basic/ads/App.kt
@@ -1,15 +1,15 @@
 package app.lexilabs.example.basic.ads
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import app.lexilabs.basic.ads.*
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -24,56 +24,69 @@ fun App(platformContext: ContextFactory) {
         var showRewardedInterstitial by remember { mutableStateOf(false) }
         var rewardCount by remember { mutableStateOf(0) }
 
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+        Surface(
+            modifier = Modifier
+                .windowInsetsPadding(WindowInsets.systemBars)
         ) {
-            Button(
-                onClick = { showBanner = !showBanner },
-            ) {
-                Text( if (showBanner) { "Hide Banner Ads" } else { "Show Banner Ads" } )
-            }
-
-            Button(
-                onClick = { showInterstitial = true },
-            ){ Text("Show Interstitial Ad") }
-
-            Text("Reward Count: $rewardCount")
-            Button(
-                onClick = { showRewarded = true },
-            ){ Text("Show Rewarded Ad") }
-
-            Button(
-                onClick = {showRewardedInterstitial = true },
-            ){ Text("Show Rewarded Interstitial Ads") }
-        }
-
-        if (showBanner) {
             Column(
-                verticalArrangement = Arrangement.SpaceBetween,
-                modifier = Modifier.fillMaxSize()
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                BannerAd()
-                BannerAd()
+                Button(
+                    onClick = { showBanner = !showBanner },
+                ) {
+                    Text(
+                        if (showBanner) {
+                            "Hide Banner Ads"
+                        } else {
+                            "Show Banner Ads"
+                        }
+                    )
+                }
+
+                Button(
+                    onClick = { showInterstitial = true },
+                ) { Text("Show Interstitial Ad") }
+
+                Text("Reward Count: $rewardCount")
+                Button(
+                    onClick = { showRewarded = true },
+                ) { Text("Show Rewarded Ad") }
+
+                Button(
+                    onClick = { showRewardedInterstitial = true },
+                ) { Text("Show Rewarded Interstitial Ads") }
             }
-        }
-        if (showInterstitial) {
-            InterstitialAd(platformContext.getActivity(), onDismissed = { showInterstitial = false } )
-        }
-        if (showRewarded) {
-            RewardedAd(
-                platformContext.getActivity(),
-                onDismissed = { showRewarded = false },
-                onRewardEarned = { rewardCount += 1 }
-            )
-        }
-        if (showRewardedInterstitial) {
-            RewardedInterstitialAd(
-                platformContext.getActivity(),
-                onDismissed = { showRewardedInterstitial = false },
-                onRewardEarned = { rewardCount += 1 }
-            )
+
+            if (showBanner) {
+                Column(
+                    verticalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    BannerAd()
+                    BannerAd()
+                }
+            }
+            if (showInterstitial) {
+                InterstitialAd(
+                    platformContext.getActivity(),
+                    onDismissed = { showInterstitial = false })
+            }
+            if (showRewarded) {
+                RewardedAd(
+                    platformContext.getActivity(),
+                    onDismissed = { showRewarded = false },
+                    onRewardEarned = { rewardCount += 1 }
+                )
+            }
+            if (showRewardedInterstitial) {
+                RewardedInterstitialAd(
+                    platformContext.getActivity(),
+                    onDismissed = { showRewardedInterstitial = false },
+                    onRewardEarned = { rewardCount += 1 }
+                )
+            }
         }
     }
 }
